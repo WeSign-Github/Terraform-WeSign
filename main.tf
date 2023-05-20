b181b5b885ceb30faff90ecdcbd4da241a581890
kamize <talithanabila4c@gmail.com>
2023-05-20 17:28:34 +0700

    initialize: IAM, artifact registry, cloud storage, and terraform version configurations

---
+++ main.tf
@@ -0,0 +1,125 @@
+# CONFIGURE GCP PROVIDERS
+provider "google" {
+  credentials = file("credentials.json")
+  project = var.project_id
+  region  = var.region
+  zone = var.zone
+}
+
+# IAM CONFIGURATION
+module "billing-account-iam" {
+  source  = "terraform-google-modules/iam/google//modules/billing_accounts_iam"
+  billing_account_ids = var.billing_account_ids
+
+  mode = var.billing_accounts_iam_mode
+  bindings = var.billing_accounts_iam_bindings
+}
+
+module "cloud-run-services-iam-bindings" {
+  source             = "terraform-google-modules/iam/google//modules/cloud_run_services_iam"
+  project            = var.project_id
+  cloud_run_services = var.cloud_run_services_iam_bindings_services
+  mode               = var.cloud_run_services_iam_bindings_mode
+
+  bindings = var.cloud_run_services_iam_bindings
+}
+
+module "artifact-registry-repository-iam-bindings" {
+  source   = "terraform-google-modules/iam/google//modules/artifact_registry_iam"
+  project      = var.project_id
+  repositories = var.artifact_registry_iam_bindings_repositories
+  location     = var.location
+  mode         = var.artifact_registry_iam_bindings_mode
+
+  bindings = var.artifact_registry_iam_bindings
+}
+
+module "storage_buckets_iam_bindings" {
+  source  = "terraform-google-modules/iam/google//modules/storage_buckets_iam"
+  version = "~> 8.0"
+
+  storage_buckets = var.storage_buckets_iam_bindings_buckets
+
+  mode = var.storage_buckets_iam_bindings_mode
+
+  bindings = var.storage_buckets_iam_bindings
+}
+
+# ARTIFACT REGISTRY
+resource "google_artifact_registry_repository" "api-repo" {
+  location      = var.location
+  repository_id = var.artifact_registry_repository_id
+  description   = "Docker repository for API"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = false
+  }
+}
+
+# CLOUD STORAGE
+module "gcs_buckets" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "~> 4.0"
+  project_id  = var.project_id
+  names = ["first", "second"]
+  prefix = "my-unique-prefix"
+  set_admin_roles = true
+  admins = ["group:foo-admins@example.com"]
+  versioning = {
+    first = false
+  }
+  bucket_admins = {
+    second = "user:spam@example.com,eggs@example.com"
+  }
+}
+
+# module "bucket" {
+#   source = "../../modules/simple_bucket"
+
+#   name       = "${var.project_id}-bucket"
+#   project_id = var.project_id
+#   location   = "us"
+
+#   lifecycle_rules = [{
+#     action = {
+#       type = "Delete"
+#     }
+#     condition = {
+#       age            = 365
+#       with_state     = "ANY"
+#       matches_prefix = var.project_id
+#     }
+#   }]
+
+#   custom_placement_config = {
+#     data_locations : ["US-EAST4", "US-WEST1"]
+#   }
+
+#   iam_members = [{
+#     role   = "roles/storage.objectViewer"
+#     member = "group:test-gcp-ops@test.blueprints.joonix.net"
+#   }]
+# }
+
+# CLOUD SQL
+
+# CLOUD RUN SERVICES
+# resource "google_cloud_run_service" "default" {
+#   name     = "wesign-cloudrun"
+#   location = process.env.LOCATION
+
+#   template {
+#     spec {
+#       containers {
+#         # TODO
+#         image = "us-docker.pkg.dev/cloudrun/container/hello"
+#       }
+#     }
+#   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
---
+++ terraform-example.tfvars
@@ -0,0 +1,90 @@
+# MAIN PROVIDER CONFIGURATION
+# This section use for addressing project and location
+# for Cloud Provider : Google Cloud Platform (GCP)
+# PROJECT
+STATUS="development"
+project_id="<project-id>"
+
+# LOCATIONS
+PORT="8000"
+ZONE="asia-southeast2"
+REGION="asia-southeast2"
+LOCATION="asia-southeast2"
+
+# IAM CONFIGURATION
+# This section is use for all IAM accounts configuration.
+# BILLING ACCOUNT
+billing_account_iam_mode="additive"
+billing_account_ids=["XXXXXX-XXXXXX-XXXXXX"]
+billing_account_iam_bindings= {
+    "roles/billing.viewer" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+    ]
+
+    "roles/billing.user" = [
+      "user:my-user@my-org.com",
+    ]
+  }
+
+# CLOUD RUN SERVICES
+cloud_run_services_iam_bindings_mode="authoritative"
+cloud_run_services_iam_bindings_services=["my_cloud_run_service_one", "my_cloud_run_service_two"]
+cloud_run_services_iam_bindings={
+    "roles/run.invoker" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+      "user:my-user@my-org.com",
+    ]
+    "roles/run.admin" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+      "user:my-user@my-org.com",
+    ]
+  }
+
+# ARTIFACT REGISTRY REPOSITORY
+artifact_registry_repository_iam_bindings_mode="additive"
+artifact_registry_repository_iam_bindings_repositories=["my-project_one", "my-project_two"]
+artifact_registry_repository_iam_bindings={
+    "roles/compute.networkAdmin" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+      "user:my-user@my-org.com",
+    ]
+  }
+
+# STORAGE BUCKETS
+storage_buckets_iam_bindings_buckets=["my-storage-bucket"]
+storage_buckets_iam_bindings_mode="authoritative"
+storage_buckets_iam_bindings={
+    "roles/storage.legacyBucketReader" = [
+      "user:josemanuelt@google.com",
+      "group:test_sa_group@lnescidev.com",
+    ]
+
+    "roles/storage.legacyBucketWriter" = [
+      "user:josemanuelt@google.com",
+      "group:test_sa_group@lnescidev.com",
+    ]
+  }
+
+# INFRASTRUCTURE CONFIGURATION
+#  This section is being use to address 
+#  all cloud infrastructure being used in Google Cloud Platform.
+
+# ARTIFACT REGISTRY 
+#  To Create gcr.io or compiling image to be use in Google Cloud Run.
+artifact_registry_repository_id="api-repo"
+
+# CLOUD RUN
+
+# STORAGE CONFIGURATION
+# CLOUD STORAGE
+gcs_buckets_names=["public-bucket"]
+cloud_storage_admins=[]
+gcs_buckets_admins={}
+
+# CLOUD SQL
+
+
